b48ceb28a2e8ccaf1c191101c1a85a03d40c8c45
YutoMina <e1b20096@st.oit.ac.jp>
2022-10-18 13:56:57 +0900

    初期化

---
+++ src/main/java/oit/is/group2/lec4/group2lec4/Group2lec4Application.java
@@ -0,0 +1,13 @@
+package oit.is.group2.lec4.group2lec4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2lec4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2lec4Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group2/lec4/group2lec4/Group2lec4ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group2.lec4.group2lec4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2lec4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
